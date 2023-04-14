d44a260e6ae2941b56549a41e78f0feffff32f52
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-14 14:41:28 -0500

    MyFirtClass.java

---
+++ src/MyFirstClass.java
@@ -0,0 +1,12 @@
+public class MyFirstClass {
+
+    //shortcut: psvm + tab or main + tab
+    public static void main(String[] args){
+        //This will log stuff in your command line
+        // Strings in Java must be in double quotes, because single quotes are reserved for characters
+        // you must put a semicolon on every statement or else you will get a syntax error
+        //shortcut is south + tab
+        System.out.println("Hello, welcome to Java!");
+
+    }
+}
