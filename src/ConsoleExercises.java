35829939c4f73e46fe5f800a11f742a30a5f80ab
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-17 12:25:02 -0500

    First Commit

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+public class ConsoleExercises {
+
+    public static void main (String[] args){
+
+        double pi = 3.14159;
+
+        System.out.printf("The value of pi is %s.%n", pi);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+
+        // The user enters an integer
+
+        int userInput = scan.nextInt();
+
+        System.out.printf("You entered %s",userInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
