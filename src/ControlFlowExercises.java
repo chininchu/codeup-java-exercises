05ac92408e377ae10a12ba5575fc9898b33cf1b8
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-18 11:50:57 -0500

    Loop Basics Final Commit

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,98 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+
+//        1. Loop Basics
+//
+//        a. While
+//
+//        Create an integer variable i with a value of 5.
+//        Create a while loop that runs so long as i is less than or equal to 15
+//        Each loop iteration, output the current value of i, then increment i by one.
+//        Your output should look like this:
+//
+//        5 6 7 8 9 10 11 12 13 14 15
+
+
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+//
+//        b. Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
+//        int i = 2;
+//
+//        do {
+//
+//            System.out.println(i);
+//            i += 2;
+//
+//        } while (i <= 100);
+
+//        Alter your loop to count backwards by 5's from 100 to -10.
+
+//        int i = 100;
+//
+//        do {
+//
+//            System.out.println(i);
+//            i -= 5;
+//
+//        } while (i >= -10);
+
+
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+
+//        long i = 2;
+//
+//        do {
+//
+//
+//            System.out.println(i);
+//            i *= i;
+//
+//
+//        } while (i < 1000000);
+
+
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
+
+
+//
+//
+//        c. For
+//
+//        Refactor the previous two exercises to use a for loop instead.
+
+//        for(long i = 5; i <= 15; i ++) {
+//            System.out.println("i is " + i);
+//        }
+
+
+//        for(long i = 2; i <= 100; i+=2) {
+//            System.out.println("i is " + i);
+//        }
+
+//        for(long i = 100; i >= -10; i-=5) {
+//            System.out.println("i is " + i);
+//        }
+
+//        for(long i = 2; i < 1000000; i*= i) {
+//            System.out.println("i is " + i);
+//        }
+
+
+
+
+    }
+}
