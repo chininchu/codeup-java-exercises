1ad8c53218bbaeef40bc0a72b6aca4e69da6c120
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-21 17:26:52 -0500

    OOP First Commit

---
+++ src/util/Input.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner scanner;
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
