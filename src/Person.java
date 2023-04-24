ff7a1622faab6b014b934c19b20c7eea769ff0a7
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-24 18:37:52 -0500

    Input Exercise

--- src/Person.java
+++ src/Person.java
@@ -10,6 +10,7 @@ public class Person {
 //TODO: return the person's name
         return name;
 
+
     }
 
     public void setName(String name){
@@ -31,17 +32,18 @@ public class Person {
     }
 
     public static void main(String[] args) {
-//
+
+
 //        Person person1 = new Person("John");
 //        Person person2 = new Person("John");
 //        System.out.println(person1.getName().equals(person2.getName()));
 //        System.out.println(person1 == person2);
-
+//
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
 //        System.out.println(person1 == person2);
-
-
+//
+//
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
 //        System.out.println(person1.getName());
