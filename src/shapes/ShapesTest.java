ead28c742d088a581df440d3c9eb96cafccaef29
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-27 11:53:43 -0500

    Created an Interface named Measurable

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,7 +5,7 @@ public class ShapesTest {
 
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(4, 5);
+        Rectangle box1 = new Rectangle(4,5);
 
         System.out.println(box1.getPerimeter());
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,26 +2,26 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    public double side;
-
-
-    public Square(double side) {
-
-        super(side, side);
-        this.side = side;
-
-    }
-
-    @Override
-    public double getArea() {
-        return Math.pow(side, 2);
-
-    }
-
-
-    @Override
-    public double getPerimeter() {
-        return 4 * side;
-
-    }
+//    public double side;
+//
+//
+//    public Square(double side) {
+//
+//        super(side, side);
+//        this.side = side;
+//
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return Math.pow(side, 2);
+//
+//    }
+//
+//
+//    @Override
+//    public double getPerimeter() {
+//        return 4 * side;
+//
+//    }
 }
