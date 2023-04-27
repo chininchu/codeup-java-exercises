157a94eace1300c2af497cd2903cc34b6122aed5
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-27 16:52:58 -0500

    Collections First Commit

---
+++ src/grades/StudentTest.java
@@ -0,0 +1,15 @@
+package grades;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Andrew");
+        student1.addGrade(30);
+        student1.addGrade(20);
+        student1.addGrade(10);
+        System.out.println(student1.getGradeAverage());
+
+
+    }
+}
